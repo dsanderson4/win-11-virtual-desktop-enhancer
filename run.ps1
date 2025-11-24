2b825f572c9c641c6e09da476f7beb3d3b880fdd
Dave Anderson <dave.anderson@dominionvoting.com>
2025-11-24 09:02:35 -0700

    Changed directory in run script.

--- run.ps1
+++ run.ps1
@@ -1,1 +1,1 @@
-& "C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe" "$env:USERPROFILE\source\repos\win-11-virtual-desktop-enhancer\virtual-desktop-enhancer.ah2"
+& "C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe" "$env:USERPROFILE\win-11-virtual-desktop-enhancer\virtual-desktop-enhancer.ah2"
